c244e61306fc1bdcd8d7611f0dcdeb060362a038
Devin Ragotzy <devin.ragotzy@gmail.com>
2020-08-18 16:26:03 -0400

    Split append_pdu -> append_pdu and build_and_append
    Move all state event appending to append_state_pdu.

--- src/client_server/account.rs
+++ src/client_server/account.rs
@@ -327,7 +327,7 @@ pub fn deactivate_route(
             third_party_invite: None,
         };
 
-        db.rooms.append_pdu(
+        db.rooms.build_and_append_pdu(
             PduBuilder {
                 room_id: room_id.clone(),
                 sender: sender_id.clone(),
