8f4f3314c6c24ca9eb54fc25927d4eb186b27115
Jakub Kubík <jakub.kubik.it@protonmail.com>
2022-07-10 14:37:34 +0200

    refactor(edus): split edus into separate modules

---
+++ src/service/rooms/edus/read_receipt/data.rs
@@ -0,0 +1,31 @@
+pub trait Data {
+    /// Replaces the previous read receipt.
+    fn readreceipt_update(
+        &self,
+        user_id: &UserId,
+        room_id: &RoomId,
+        event: ReceiptEvent,
+    ) -> Result<()>;
+
+    /// Returns an iterator over the most recent read_receipts in a room that happened after the event with id `since`.
+    fn readreceipts_since(
+        &self,
+        room_id: &RoomId,
+        since: u64,
+    ) -> impl Iterator<
+        Item = Result<(
+            Box<UserId>,
+            u64,
+            Raw<ruma::events::AnySyncEphemeralRoomEvent>,
+        )>,
+    >;
+
+    /// Sets a private read marker at `count`.
+    fn private_read_set(&self, room_id: &RoomId, user_id: &UserId, count: u64) -> Result<()>;
+
+    /// Returns the private read marker.
+    fn private_read_get(&self, room_id: &RoomId, user_id: &UserId) -> Result<Option<u64>>;
+
+    /// Returns the count of the last typing update in this room.
+    fn last_privateread_update(&self, user_id: &UserId, room_id: &RoomId) -> Result<u64>;
+}
