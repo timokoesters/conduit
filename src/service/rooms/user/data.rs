3a09c8e1801605e60c45739c9348ceab5bc4a85f
Timo Kösters <timo@koesters.xyz>
2022-08-14 13:38:21 +0200

    refactor state accessor, state cache, user, uiaa

---
+++ src/service/rooms/user/data.rs
@@ -0,0 +1,21 @@
+pub trait Data {
+    fn reset_notification_counts(&self, user_id: &UserId, room_id: &RoomId) -> Result<()>;
+
+    fn notification_count(&self, user_id: &UserId, room_id: &RoomId) -> Result<u64>;
+
+    fn highlight_count(&self, user_id: &UserId, room_id: &RoomId) -> Result<u64>;
+
+    fn associate_token_shortstatehash(
+        &self,
+        room_id: &RoomId,
+        token: u64,
+        shortstatehash: u64,
+    ) -> Result<()>;
+
+    fn get_token_shortstatehash(&self, room_id: &RoomId, token: u64) -> Result<Option<u64>>;
+
+    fn get_shared_rooms<'a>(
+        &'a self,
+        users: Vec<Box<UserId>>,
+    ) -> Result<impl Iterator<Item = Result<Box<RoomId>>> + 'a>;
+}
