a3716a7d5acda535359eb7ef9145b98edfbe8487
Matthias Ahouansou <matthias@ahouansou.cz>
2024-08-28 11:24:51 +0100

    chore: upgrade request client matrix version
    this is needed so that new endpoints use stable paths

--- src/api/server_server.rs
+++ src/api/server_server.rs
@@ -207,7 +207,7 @@ where
         .try_into_http_request::<Vec<u8>>(
             &actual_destination_str,
             SendAccessToken::IfRequired(""),
-            &[MatrixVersion::V1_4],
+            &[MatrixVersion::V1_11],
         )
         .map_err(|e| {
             warn!(
